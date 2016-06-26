e5b1b917314fea4e7069946e57a11d6026d74df5
Alexandra Dedinska <alex.dedinska@gmail.com>
2016-06-26 12:12:00 +0200

    Added more granularity to preparation times.

--- src/main/java/cz/afrosoft/whattoeat/logic/model/enums/PreparationTime.java
+++ src/main/java/cz/afrosoft/whattoeat/logic/model/enums/PreparationTime.java
@@ -12,8 +12,25 @@ import cz.afrosoft.whattoeat.gui.Labeled;
  * @author Alexandra
  */
 public enum PreparationTime implements Labeled {
+    /**
+     * Up to 3 minutes. Like pick something from shelve.
+     */
+    INSTANT("cz.afrosoft.whattoeat.preparationTime.instant"),
+    /**
+     * Up to 10 minutes.
+     */
+    QUICK("cz.afrosoft.whattoeat.preparationTime.quick"),
+    /**
+     * Up to 30 minutes.
+     */
     SHORT("cz.afrosoft.whattoeat.preparationTime.short"),
+    /**
+     * Up to 1 hour.
+     */
     MEDIUM("cz.afrosoft.whattoeat.preparationTime.medium"),
+    /**
+     * More than hour.
+     */
     LONG("cz.afrosoft.whattoeat.preparationTime.long");
 
     private final String labelKey;
