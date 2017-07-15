be87a36c481da4fee7e6fd5e0c0bfcf0e4767048
Tomas Rejent <rejent.tomas@gmail.com>
2017-07-15 13:12:28 +0200

    Updated Taste to be IdEnum and created converter.

--- src/main/java/cz/afrosoft/whattoeat/cookbook/recipe/logic/model/Taste.java
+++ src/main/java/cz/afrosoft/whattoeat/cookbook/recipe/logic/model/Taste.java
@@ -1,30 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.afrosoft.whattoeat.cookbook.recipe.logic.model;
 
+import cz.afrosoft.whattoeat.core.data.IdEnum;
 import cz.afrosoft.whattoeat.core.gui.Labeled;
+import org.apache.commons.lang3.Validate;
 
 /**
+ * Indicates taste of recipe. Recipe can have only one taste.
  *
- * @author Alexandra
+ * @author Tomas Rejent
  */
-public enum Taste implements Labeled {
-    SWEET("cz.afrosoft.whattoeat.taste.sweet"),
-    SALTY("cz.afrosoft.whattoeat.taste.salty"),
-    NEUTRAL("cz.afrosoft.whattoeat.taste.neutral");
+public enum Taste implements IdEnum, Labeled {
+    /**
+     * Sweet taste. Typically meals consisting of sugar or honey.
+     */
+    SWEET(1, "cz.afrosoft.whattoeat.taste.sweet"),
+    /**
+     * Salty taste. Typical for meat meals.
+     */
+    SALTY(2, "cz.afrosoft.whattoeat.taste.salty"),
+    /**
+     * Neutral taste. For example bread, which can be consumed with both sweet or salty additions.
+     */
+    NEUTRAL(3, "cz.afrosoft.whattoeat.taste.neutral");
 
+    private final int id;
     private final String labelKey;
 
-    private Taste(String labelKey){
+    Taste(final int id, final String labelKey) {
+        Validate.notBlank(labelKey);
+        this.id = id;
         this.labelKey = labelKey;
+    }
+
+    @Override
+    public int getId() {
+        return id;
     }
 
     @Override
     public String getLabelKey() {
         return labelKey;
     }
-
 }
