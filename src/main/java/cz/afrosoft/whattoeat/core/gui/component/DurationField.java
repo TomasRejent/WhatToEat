840c786d8a20b297ca686d9ad6be76ea7c529616
Tomas Rejent <rejent.tomas@gmail.com>
2020-11-02 10:11:26 +0100

    Fixed regex for duration field so it allows zero value.

--- src/main/java/cz/afrosoft/whattoeat/core/gui/component/DurationField.java
+++ src/main/java/cz/afrosoft/whattoeat/core/gui/component/DurationField.java
@@ -14,7 +14,7 @@ public class DurationField extends RegexRestrictedField {
 
     private static final String PLACEHOLDER_KEY = "cz.afrosoft.whattoeat.component.durationfields.placeholder";
 
-    private static final String VALIDATION_REGEX = "^$|^([1-9])+[0-9]*$";
+    private static final String VALIDATION_REGEX = "^$|^0$|^([1-9])+[0-9]*$";
 
     public DurationField() {
         super(VALIDATION_REGEX);
